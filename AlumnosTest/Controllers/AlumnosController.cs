2eb9d9f5817a434e83c6c936bb262d4cf0d34a26
Gerson Azabache <gerson_dunk@hotmail.com>
2021-08-17 15:15:24 -0500

    second controller and first one-to-many relationship added

--- AlumnosTest/Controllers/AlumnosController.cs
+++ AlumnosTest/Controllers/AlumnosController.cs
@@ -22,13 +22,51 @@ namespace AlumnosTest.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Alumno>>> Get()
         {
-            return await context.Alumnos.ToListAsync();
+            return await context.Alumnos.Include(x=>x.Matriculas).ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Alumno>> Get(int id)
+        {
+            var existe = await context.Alumnos.AnyAsync(x => x.Id == id);
+            if (!existe)
+                return NotFound($"No existe el alumno con id: {id}");
+
+            return await context.Alumnos.FirstOrDefaultAsync(x=>x.Id == id);
         }
 
         [HttpPost]
         public async Task<ActionResult> Post(Alumno alumno)
         {
             context.Alumnos.Add(alumno);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<ActionResult> Put(int id, Alumno alumno)
+        {
+            var existe = await context.Alumnos.AnyAsync(x => x.Id == id);
+            if (!existe)
+                return NotFound($"No existe el alumno con id: {id}");
+
+            if (id != alumno.Id)
+                return BadRequest("Los id no coinciden");
+
+            context.Alumnos.Update(alumno);
+            await context.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {            
+            var existe = await context.Alumnos.AnyAsync(x => x.Id == id);
+            if (!existe)
+                return NotFound($"No existe el alumno con id: {id}");
+
+            context.Alumnos.Remove(new Alumno { Id = id });
             await context.SaveChangesAsync();
             return Ok();
         }
---
+++ AlumnosTest/Controllers/MatriculasController.cs
@@ -0,0 +1,35 @@
+﻿using AlumnosTest.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlumnosTest.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MatriculasController:ControllerBase
+    {
+        private readonly AlumnosTestContext context;
+
+        public MatriculasController(AlumnosTestContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<ActionResult<List<Matricula>>> Get()
+        {
+            return await context.Matriculas.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Matricula matricula)
+        {
+            context.Matriculas.Add(matricula);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
